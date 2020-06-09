7a1f4ea40fb8f9a3b41842a33eb87be4bf006322
Cloud User <centos@ip-172-31-2-149.eu-west-2.compute.internal>
2020-06-09 14:37:39 +0000

    Solution Ex4

--- Ex4/top.v
+++ Ex4/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #4 
-// Student Name:
-// Date: 
+// Student Name: Bernabeu Antoine
+// Date: 09/06/2020
 //
 //  Description: In this exercise, you need to design an electronic dice, following
 //  the diagram provided in the exercises documentation. The dice rolls as long as
@@ -16,3 +16,35 @@
 //  You need to write the whole file.
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module dice (
+        input clk,
+        input rst,
+        input button,
+        output reg[2:0] throw
+            );
+initial
+begin
+	throw<=3'b000;
+end
+always @(clk,rst,button)
+begin
+    if(rst)
+        throw<=3'b000;
+     else
+        if(throw==3'b000 || throw==3'b111)
+        begin
+            throw<=3'b001;
+        end
+        else
+            if(button==1'b1)
+            begin
+            throw<=throw+1'b1;
+            end
+end
+
+endmodule
+
+
+    
--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -1,9 +1,109 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #4 - Electronic Dice
-// Student Name:
-// Date: 
+// Student Name: Bernabeu Antoine
+// Date: 09/06/2020
 //
 // Description: A testbench module to test Ex4 - Electronic Dice
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module top_tb(
+    );
+
+    //Todo: Parameters
+    parameter CLK_PERIOD = 10;
+
+    //Todo: Regitsers and wires
+    reg clk;
+    reg rst;
+    reg button;
+    
+    reg err;
+    reg [2:0]throw_prev;
+    wire [2:0] throw;
+	
+    //Todo: Clock generation
+    initial
+    begin
+       clk = 1'b0;
+       forever
+         #(CLK_PERIOD/2) clk=~clk;
+    end
+
+    //Todo: User logic
+    initial
+    begin
+        rst=0;
+        err=0;
+        button=0;
+        #15
+        //Testing Initialisation
+        if(throw!=3'b001)
+	   begin
+		  $display("***TEST_FAILED! INIT");
+		  err=1;
+	   end
+        //Testing incrementation
+        forever begin
+        button=1;
+        rst=0;
+        throw_prev=throw;
+        #CLK_PERIOD  
+        if(throw_prev==3'b111)
+            if(throw!=3'b001)
+            begin
+                $display("***TEST_FAILED! after 7, not going to 1");
+                err=1;
+            end 
+        else
+            if(throw!=throw_prev+1)
+            begin
+                $display("***TEST_FAILED! increment");
+                err=1;
+            end
+        #10
+        rst=1;
+        #CLK_PERIOD
+        if(throw!=3'b000)
+        begin
+            $display("***TEST_FAILED reset");
+            err=1;
+        end
+        rst=0;
+        #5
+        throw_prev=throw;
+        button=0;
+        #CLK_PERIOD
+        if(throw_prev==3'b000 ||throw_prev==3'b111)
+            if(throw!=3'b001)
+            begin
+                $display("***TEST_FAILED! edge with button 0");
+                err=1;
+            end
+        else
+            if(throw!=throw_prev)
+            begin
+                $display("***TEST_FAILED freeze dice");
+                err=1;
+            end   
+       end
+   end    	
+    //Todo: Finish test, check for success
+    initial 
+    begin
+       #100
+       if (err==0)
+         $display("***TEST PASSED! :) ***");
+       $finish;
+    end
+
+	//Todo: Instantiate counter module
+	dice top(
+ 	.clk (clk),
+ 	.rst (rst),
+ 	.button (button),
+ 	.throw (throw)
+ 	);
+endmodule
